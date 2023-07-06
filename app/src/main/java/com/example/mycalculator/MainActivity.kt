41e9f270338b89e472a9e36ce10b04bdefd61054
wamboimargaret <margarettwamboi@gmail.com>
2023-07-06 08:19:41 +0300

    my calculator

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -5,82 +5,62 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.example.mycalculator.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
-    lateinit var tvCalculator:TextView
-    lateinit var btnAdd: Button
-    lateinit var btnSubtract:Button
-    lateinit var btnMultiply: Button
-    lateinit var btnModules: Button
-    lateinit var tilFirstNumber:TextInputLayout
-    lateinit var tilSecondNumber: TextInputLayout
-    lateinit var etFirstNumber: TextInputEditText
-    lateinit var etSecondNumber: TextInputEditText
-    lateinit var tvResult: TextView
+    lateinit var binding: ActivityMainBinding
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        castViews()
-
-
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onResume() {
         super.onResume()
-        btnAdd.setOnClickListener {
+        binding.btAdd.setOnClickListener {
             addition()
         }
-        btnSubtract.setOnClickListener {
+        binding.btSubtract.setOnClickListener {
             subtraction()
         }
-        btnMultiply.setOnClickListener {
+        binding.btMultiply.setOnClickListener {
             multiplication()
         }
-        btnModules.setOnClickListener {
+        binding.btModules.setOnClickListener {
             division()
         }
     }
-    fun castViews(){
-        tvCalculator = findViewById(R.id.tvCalculator)
-        tilFirstNumber= findViewById(R.id.tilFirstNumber)
-        tilSecondNumber = findViewById(R.id.tilSecondNumber)
-        etFirstNumber=findViewById(R.id.etFirstNumber)
-        etSecondNumber=findViewById(R.id.etSecondNumber)
-        btnAdd=findViewById(R.id.btAdd)
-        btnSubtract=findViewById(R.id.btSubtract)
-        btnModules=findViewById(R.id.btModules)
-        btnMultiply=findViewById(R.id.btMultiply)
-        tvResult=findViewById(R.id.tvResult)
-    }
-    fun addition(){
-        val firstNumber=etFirstNumber.text.toString().toDouble()
-        val secondNumber=etSecondNumber.text.toString().toDouble()
-        val answer = firstNumber+secondNumber
-        tvResult.text=answer.toString()
+
+    fun addition() {
+        val firstNumber = binding.etFirstNumber.text.toString().toDouble()
+        val secondNumber = binding.etSecondNumber.text.toString().toDouble()
+        val answer = firstNumber + secondNumber
+        binding.tvResult.text = answer.toString()
 
     }
-    fun subtraction(){
-        val firstNumber=etFirstNumber.text.toString().toDouble()
-        val secondNumber=etSecondNumber.text.toString().toDouble()
-        val answer=firstNumber-secondNumber
-        tvResult.text=answer.toString()
+
+    fun subtraction() {
+        val firstNumber = binding.etFirstNumber.text.toString().toDouble()
+        val secondNumber = binding.etSecondNumber.text.toString().toDouble()
+        val answer = firstNumber - secondNumber
+        binding.tvResult.text = answer.toString()
     }
 
-    fun multiplication(){
-        val firstNumber=etFirstNumber.text.toString().toDouble()
-        val secondNumber=etSecondNumber.text.toString().toDouble()
-        val answer=firstNumber * secondNumber
-        tvResult.text=answer.toString()
+    fun multiplication() {
+        val firstNumber = binding.etFirstNumber.text.toString().toDouble()
+        val secondNumber = binding.etSecondNumber.text.toString().toDouble()
+        val answer = firstNumber * secondNumber
+        binding.tvResult.text = answer.toString()
     }
-    fun division(){
-        val firstNumber=etFirstNumber.text.toString().toDouble()
-        val secondNumber=etSecondNumber.text.toString().toDouble()
-        val answer=firstNumber % secondNumber
-        tvResult.text=answer.toString()
+
+    fun division() {
+        val firstNumber = binding.etFirstNumber.text.toString().toDouble()
+        val secondNumber = binding.etSecondNumber.text.toString().toDouble()
+        val answer = firstNumber % secondNumber
+        binding.tvResult.text = answer.toString()
     }
 }
